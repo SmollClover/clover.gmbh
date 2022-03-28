ad418a601758cc0b82d9a85d5a8cf05b07b6efff
Clover <39840298+SmollClover@users.noreply.github.com>
2022-03-28 20:35:42 +0200

    [Navbar] Basic Navbar and Layout done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css';
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 
+import Sidebar from '../components/Sidebar';
+
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	useEffect(() => {
+		document.body.classList.add('bg-darker');
+		document.body.classList.add('text-white');
+	});
+
+	return (
+		<div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14 sm:px-20 md:px-32">
+			<div className="col-span-12 p-4 text-center lg:col-span-3 bg-dark rounded-2xl">
+				<Sidebar />
+			</div>
+			<div className="col-span-12 lg:col-span-9 bg-dark rounded-2xl">
+				<Component {...pageProps} />
+			</div>
+		</div>
+	);
 }
 
 export default MyApp;
