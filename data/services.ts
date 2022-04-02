7cad50fba6152c8eb966fef6f1c07f9a88a1d6fa
Clover <39840298+SmollClover@users.noreply.github.com>
2022-04-02 14:04:47 +0200

    Updated Services

--- data/services.ts
+++ data/services.ts
@@ -1,36 +1,26 @@
-import { MdComputer } from 'react-icons/md';
+import { FaCloud, FaRegWindowRestore, FaRobot, FaTools } from 'react-icons/fa';
 
 import { Service } from '../interface/Service';
 
 export const Services: Array<Service> = [
 	{
-		title: 'Test Title',
-		description: 'Test Description',
-		SIcon: MdComputer,
+		title: 'Discord Bots',
+		description: 'I\'ve been programming Discord Bots for 4 years now, so I\'ve learned how to properly do it.',
+		SIcon: FaRobot,
 	},
 	{
-		title: 'Test Title',
-		description: 'Test Description',
-		SIcon: MdComputer,
+		title: 'Websites',
+		description: 'I\'m capable of building very small Websites that serve simple purposes.',
+		SIcon: FaCloud,
 	},
 	{
-		title: 'Test Title',
-		description: 'Test Description',
-		SIcon: MdComputer,
+		title: 'Applications',
+		description: 'Through Electron, I can make applications for Windows, Linux and MacOS.',
+		SIcon: FaRegWindowRestore,
 	},
 	{
-		title: 'Test Title',
-		description: 'Test Description',
-		SIcon: MdComputer,
-	},
-	{
-		title: 'Test Title',
-		description: 'Test Description',
-		SIcon: MdComputer,
-	},
-	{
-		title: 'Test Title',
-		description: 'Test Description',
-		SIcon: MdComputer,
+		title: 'Tools',
+		description: 'Small Applications which do certain actions by parsing starting Arguments.',
+		SIcon: FaTools,
 	},
 ];
